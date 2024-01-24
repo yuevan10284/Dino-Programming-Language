aadfdfa2e076ebc1957a3504e94f217beeb9a8a3
Anthony Mendizabal <amendiz2@lion.lmu.edu>
2024-01-24 15:04:29 -0800

    src & test folders

---
+++ test/compiler.test.js
@@ -0,0 +1,8 @@
+import * as assert from "node:assert";
+import { describe } from "node:test";
+
+describe("Complier", () => {
+  it("should complie", () => {
+    assert.strictEqual(1 + 1, 2);
+  });
+});
