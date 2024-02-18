246c26be132db926143b1e723763a7c00afbfe2f
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2024-02-18 13:19:46 -0800

    Add files via upload

--- test/compiler.test.js
+++ test/compiler.test.js
@@ -1,10 +1,35 @@
-import * as assert from "node:assert";
-import { describe } from "node:test";
-import {compile} from "../src/rawrdino.js"
+import assert from "assert/strict";
+import util from "util";
+import compile from "../src/compiler.js";
+import optimize from "../src/optimizer.js";
 
-describe("Complier", () => {
-  it("should complie", () => {
-    assert.equal(compile(), "eventually this will return the compiled code");
-    assert.strictEqual(1 + 1, 2);
+const testProgram = 'rawr(0)';
+
+describe("The compiler", () => {
+  it("throws err when the output type is unknown", (done) => {
+    assert.throws(
+      () => compile("rawr(0)", "blah"),
+      /Unknown output type/
+    );
+    done();
+  });
+  it("accepts the analyzed option", (done) => {
+    const compiled = compile(testProgram, "analyzed");
+    assert(util.format(compiled).startsWith("   1 | Program"));
+    done();
+  });
+  it("accepts the optimized option", (done) => {
+    const compiled = compile(testProgram, "optimized");
+    assert(util.format(compiled).startsWith("   1 | Program"));
+    done();
+  });
+  it("generates js code when given the js option", (done) => {
+    const compiled = compile(testProgram, "js");
+    assert(compiled.startsWith(`console.log("A cup of coffee")`));
+    done();
+  });
+  it("throw when optimizer is not done", (done) => {
+    assert.throws(() => optimize());
+    done();
   });
 });
