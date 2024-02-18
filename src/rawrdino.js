59a83491107bf11b9ff7aea5ec6ee6277f01c3bf
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2024-02-18 13:07:42 -0800

    Add files via upload

--- src/rawrdino.js
+++ src/rawrdino.js
@@ -1,5 +1,32 @@
-console.log("Just getting started");
+#! /usr/bin/env node
 
-export function compile() {
-    return "eventually this will return the compiled code";
+import fs from "fs/promises"
+import process from "process"
+import compile from "./compiler.js"
+
+const help = `dino compiler
+
+Syntax: node rawrdino.js <filename> <outputType>
+
+Prints to stdout according to <outputType>, which must be :
+
+  analyzed   the semantically analyzed representation
+  optimized  the optimized semantically analyzed representation
+  js         the translation to JavaScript
+`
+
+async function compileFromFile(filename, outputType) {
+  // try {
+  const buffer = await fs.readFile(filename)
+  console.log(compile(buffer.toString(), outputType))
+  // } catch (e) {
+  //   console.error(`\u001b[31m${e}\u001b[39m`)
+  // process.exitCode = 1
+  // }
+}
+
+if (process.argv.length !== 4) {
+  console.log(help)
+} else {
+  compileFromFile(process.argv[2], process.argv[3])
 }
