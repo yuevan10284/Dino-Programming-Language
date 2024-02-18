efbd3fc9481339456f67d5d97e7e17f512a74238
Evan Yu <92699280+yuevan10284@users.noreply.github.com>
2024-02-18 13:03:53 -0800

    Add files via upload

--- src/parser.js
+++ src/parser.js
@@ -1,1 +1,11 @@
 
+import * as fs from "node:fs";
+import ohm from "ohm-js";
+
+const grammar = ohm.grammar(fs.readFileSync("rawrdino.ohm"));
+
+export default function parse(sourceCode) {
+  const match = grammar.match(sourceCode);
+  if (!match.succeeded()) throw new Error(match.message);
+  return match;
+}
