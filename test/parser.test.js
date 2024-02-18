7ff0557d112f42c0d7daa35ad481ac634c018486
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2024-02-18 13:55:42 -0800

    Add files via upload

--- test/parser.test.js
+++ test/parser.test.js
@@ -1,1 +1,45 @@
+import assert from "assert/strict"
+import fs from "fs"
+import ohm from "ohm-js"
 
+const syntaxChecks = [
+  ["all numeric literal forms", "rawr(8 * 89.123)"],
+  ["complex expressions", "rawr(83 * ((((-((((13 / 21)))))))) + 1 - 0)"],
+  ["all unary operators", "rawr (-3) rawr (!false)"],
+  ["all binary operators", "rawr x && y || z * 1 / 2 ** 3 + 4 < 5"],
+  ["all logical operators", "letdino x = true && false || (!false)"],
+  ["all arithmetic operators", "letdino x = (!3) * 2 + 4 - (-7.3) * 8 ** 13 / 1"],
+  ["all relational operators", "letdino x = 1<(2<=(3==(4!=(5 >= (6>7)))))"],
+  ["the conditional operator", "rawr x ? y : z"],
+  ["comments with no text are ok", "rawr(1)cR\nrawr(0)cR"],
+  ["end of program inside comment", "rawr(0) cR yay"],
+  ["non-Latin letters in identifiers", "コンパ = 100"],
+]
+
+const syntaxErrors = [
+  ["non-letter in an identifier", "ab😭c = 2", /Line 1, col 3/],
+  ["malformed number", "x= 2.", /Line 1, col 6/],
+  ["a missing right operand", "rawr(5 -", /Line 1, col 11/],
+  ["an expression starting with a )", "x = )", /Line 1, col 5/],
+  ["a non-operator", "rawr(7 * ((2 _ 3)", /Line 1, col 16/],
+  ["a statement starting with expression", "x * 5", /Line 1, col 3/],
+  ["an illegal statement on line 2", "rawr(5)\nx * 5", /Line 2, col 3/],
+  ["a statement starting with a )", "rawr(5)\n) * 5", /Line 2, col 1/],
+  ["an expression starting with a *", "x = * 71", /Line 1, col 5/],
+]
+
+describe("The parser", () => {
+  const grammar = ohm.grammar(fs.readFileSync("src/rawrdino.ohm"))
+  for (const [scenario, source] of syntaxChecks) {
+    it(`properly specifies ${scenario}`, () => {
+      assert(grammar.match(source).succeeded())
+    })
+  }
+  for (const [scenario, source, errorMessagePattern] of syntaxErrors) {
+    it(`does not permit ${scenario}`, () => {
+      const match = grammar.match(source)
+      assert(!match.succeeded())
+      assert(new RegExp(errorMessagePattern).test(match.message))
+    })
+  }
+})
