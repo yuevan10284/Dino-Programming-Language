2e24eb44d5e2978e118e63396816cb5eb8ecf3b3
Owen Hunger <97658961+ohunger@users.noreply.github.com>
2024-04-27 14:52:01 -0700

    start optimizer

--- src/optimizer.js
+++ src/optimizer.js
@@ -1,3 +1,30 @@
+import { unary } from "./core.js"
+
 export default function optimize(node) {
   return node
+}
+
+const optimizers = {
+  program(p) {
+    p.statements = p.statements.flatMap(optimize)
+    return p
+  },
+  variableDeclaration(d) {
+    d.initializer = optimize(d.initializer)
+    return d
+  },
+  functionDeclaration(d) {
+    d.body = optimize(d.body)
+    return d
+  },
+  /*
+  PrintStatement(s) {
+    s.argument = optimize(s.argument)
+    return s
+  },
+  */
+  returnStatement(s) {
+    s.expression = optimize(s.expression)
+    return s
+  },
 }
